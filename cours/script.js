5bf3d360f0c79adf325e5ab5997f9586b81546d9
urud.theo@gmail.com <urud.theo@gmail.com>
2024-02-22 17:11:02 +0100

    API course with TP

---
+++ cours/script.js
@@ -0,0 +1,31 @@
+// XMLHttpRequest (ancien)
+
+function reqListener() {
+  // console.log(this.responseText);
+}
+
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+// req.open("get", "data.txt", true);
+// req.open("get", "data.json", true);
+req.open("get", "https://api.blablagues.net/?rub=blagues", true);
+req.send();
+
+// -------
+// FETCH (va chercher)
+// -------
+
+// FETCH ECRITURE THEORIQUE
+
+// fetch("lien", "objets d'options")
+//   .then((response) => {
+//     // console.log(response)
+//   })
+//   .catch((err) => console.log(err));
+
+fetch("data.txt").then((res) => res.text());
+// .then((data) => console.log(data));
+
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
