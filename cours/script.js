833b38b1c9fb2e7541785f8e4641cc7e9db0f4c1
urud.theo@gmail.com <urud.theo@gmail.com>
2024-02-23 15:51:15 +0100

    work on API and json database

--- cours/script.js
+++ cours/script.js
@@ -26,6 +26,42 @@ req.send();
 fetch("data.txt").then((res) => res.text());
 // .then((data) => console.log(data));
 
-fetch("data.json")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+fetch("data.json").then((res) => res.json());
+// .then((data) => console.log(data));
+
+// de base fetch est en method GET. GET => fourni moi les données
+// On effectue une requête, APPORTE MOI DES DONNEES
+
+const myHeaders = new Headers();
+
+const init = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
+
+// fetch("data.json", init).then((res) => console.log(res));
+
+// ----------------------------
+// CRUD => Creat (POST), Read (GET), Update (PUT), Delete (DELETE)
+// Acronyme des 4 fonctions de base pour agir sur des données stockées.
+
+const init2 = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    pseudo: "FromScratch",
+    age: 42,
+  }),
+  mode: "cors",
+  credentials: "same-origin",
+};
+
+document.querySelector("form").addEventListener("submit", () => {
+  fetch("http://localhost:3000/users", init2).then(() =>
+    console.log("data envoye")
+  );
+});
